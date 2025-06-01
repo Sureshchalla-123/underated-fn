449c97485baea77fa437dccbc81bcfa651bb9a99
Sureshchalla-123 <sureshchalla9876@gmail.com>
2025-06-01 08:02:08 +0530

    fixed config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-const PORT = import.meta.env.PORT || 3000;
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    port: PORT,
-    host: true,
+    port: 3000, // only used locally
+    host: true, // allow external access in local dev if needed
   },
 });
