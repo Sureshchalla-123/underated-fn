bd36099af665752890549847521885be8ec47d4b
Sureshchalla-123 <sureshchalla9876@gmail.com>
2025-06-01 07:53:41 +0530

    fixed port issue

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+const PORT = import.meta.env.PORT || 3000;
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    port: 5173,
+    port: PORT,
+    host: true,
   },
 });
